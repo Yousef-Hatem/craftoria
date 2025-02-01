14db1f036e12236a8e32cfcfe33d5d4f43d50eb6
Yousef Hatem <yousef26hatem@gmail.com>
2025-02-01 10:36:56 +0200

    Add auth routes to app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
+import { VerifyEmailComponent } from './components/auth/verify-email/verify-email.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth/login',
+    component: LoginComponent,
+  },
+  {
+    path: 'auth/sign-up',
+    component: SignUpComponent,
+  },
+  {
+    path: 'auth/verify-email',
+    component: VerifyEmailComponent,
+  },
+];
