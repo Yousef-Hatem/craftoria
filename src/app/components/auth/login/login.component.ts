ffbd40b61fa663780132fb7cc400a2f864b793bf
Yousef Hatem <yousef26hatem@gmail.com>
2025-02-07 13:50:26 +0200

    Update Login

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { AuthService } from './../../../services/auth.service';
+import { Component, inject } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AnimationsDirective } from '../../../animations.directive';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-login',
@@ -13,8 +22,44 @@ import { AnimationsDirective } from '../../../animations.directive';
     MatButtonModule,
     RouterLink,
     AnimationsDirective,
+    MatProgressSpinnerModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loading: boolean = false;
+  errorMsg: boolean = false;
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
+  auth = inject(AuthService);
+  router = inject(Router);
+
+  login(): void {
+    if (this.form.valid) {
+      this.loading = true;
+      this.form.disable();
+
+      const { value } = this.form;
+      this.auth.login(<string>value.email, <string>value.password).subscribe(
+        (res) => {
+          this.auth.setToken(res.token);
+          this.auth.setUser(res.data);
+          this.router.navigate(['/account']);
+        },
+        (err) => {
+          if (err.status !== 401) {
+            console.log(err);
+          }
+          this.errorMsg = true;
+          this.loading = false;
+          this.form.enable();
+        },
+      );
+    }
+  }
+}
