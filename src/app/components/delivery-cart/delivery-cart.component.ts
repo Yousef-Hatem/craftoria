3b49caec75e812a83b6eaa51aa90ecce24472609
Olaa Qoutb <olaaahmed750@gmail.com>
2025-02-09 23:27:43 +0200

    home page seconde vertion

--- src/app/components/delivery-cart/delivery-cart.component.ts
+++ src/app/components/delivery-cart/delivery-cart.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-delivery-cart',
   imports: [],
   templateUrl: './delivery-cart.component.html',
-  styleUrl: './delivery-cart.component.scss',
+  styleUrl: './delivery-cart.component.css',
+
 })
 export class DeliveryCartComponent {
   hoverCard = false;
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -6,7 +6,10 @@ import { MatInputModule } from '@angular/material/input';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
+
+
 @Component({
+
   selector: 'app-nav',
   imports: [
     CommonModule,
@@ -14,23 +17,29 @@ import { CommonModule } from '@angular/common';
     MatIconModule,
     MatButtonModule,
     MatInputModule,
-    RouterModule,
+    RouterModule
+
   ],
 
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss',
+  styleUrl:    './nav.component.css'
 })
 export class NavComponent {
   menuItems = [
     { label: 'Home', route: '/home' },
     { label: 'Shop', route: '/shop' },
-  ];
 
+  ];
   searchQuery: string = '';
+  isOrdersVisible: boolean = false; // حالة ظهور الطلبات
 
   onSearch(event: Event): void {
     const inputElement = event.target as HTMLInputElement;
     this.searchQuery = inputElement.value;
     console.log('Search Query:', this.searchQuery);
+  }
+
+  toggleOrders(): void {
+    this.isOrdersVisible = !this.isOrdersVisible;
   }
 }
