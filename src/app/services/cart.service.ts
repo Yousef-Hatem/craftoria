1d4b9df1a58b23abd908dc1d827bc9a8dea019fc
Yousef Hatem <yousef26hatem@gmail.com>
2025-02-10 19:45:57 +0200

    Cart Service

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartItem } from '../interfaces/cart-item';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  constructor(private http: HttpClient) {}
+
+  getLoggedUserCart(): Observable<{ data: CartItem[] }> {
+    return this.http.get<{ data: CartItem[] }>(environment.apiUrl + '/cart');
+  }
+
+  addProductToCart(productId: string): Observable<{ data: CartItem[] }> {
+    return this.http.post<{ data: CartItem[] }>(environment.apiUrl + '/cart', {
+      productId,
+    });
+  }
+
+  updateItemQuantity(
+    productId: string,
+    quantity: number,
+  ): Observable<{ data: CartItem[] }> {
+    return this.http.put<{ data: CartItem[] }>(
+      environment.apiUrl + '/cart/' + productId,
+      { quantity },
+    );
+  }
+
+  removeItem(productId: string): Observable<{ data: CartItem[] }> {
+    return this.http.delete<{ data: CartItem[] }>(
+      environment.apiUrl + '/cart/' + productId,
+    );
+  }
+}
