e7d7487a19a4af2cad057bddff3a2f0212ce59f6
Yousef Hatem <yousef26hatem@gmail.com>
2025-02-11 21:54:11 +0200

    Fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,10 @@ import { Router, RouterOutlet } from '@angular/router';
 import { AuthLayoutComponent } from './components/auth/auth-layout/auth-layout.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    AuthLayoutComponent,
-    NavComponent,
-    FooterComponent,
-    DashboardComponent,
-  ],
+  imports: [RouterOutlet, AuthLayoutComponent, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
