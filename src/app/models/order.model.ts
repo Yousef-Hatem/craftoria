42392d58ef66f7d4b3dd0ae75b2d40eab5c2262a
Olaa Qoutb <olaaahmed750@gmail.com>
2025-02-11 22:00:08 +0200

    model

---
+++ src/app/models/order.model.ts
@@ -0,0 +1,17 @@
+export interface Order {
+  id: number;
+  customerName: string;
+  items: OrderItem[];
+  totalPrice: number;
+  status: 'shipped' | 'delivered' | 'confirmed'|'cancelled';
+  paymentMethod: 'cash' | 'visa';
+  estimatedDeliveryDays?: number;
+  shippedAt?: Date;
+}
+
+export interface OrderItem {
+  productName: string;
+  productImage: string;
+  quantity: number;
+  price: number;
+}
