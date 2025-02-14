8cb3a2d35082e549778a777532dafd022507bdc6
Yousef Hatem <yousef@yousefhatem.com>
2025-02-14 23:39:14 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { Router, RouterOutlet } from '@angular/router';
 import { AuthLayoutComponent } from './components/auth/auth-layout/auth-layout.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { CommonModule } from '@angular/common';  
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AuthLayoutComponent, NavComponent, FooterComponent],  
+  imports: [RouterOutlet, AuthLayoutComponent, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
